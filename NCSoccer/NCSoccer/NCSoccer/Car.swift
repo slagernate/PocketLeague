3760742702fd674e34d22c259ecd57233c29e095
slagernate <slagernate@gmail.com>
2016-09-18 13:41:31 -0400

    Added car.png (basic car sprite) and some controls

--- NCSoccer/NCSoccer/NCSoccer/Car.swift
+++ NCSoccer/NCSoccer/NCSoccer/Car.swift
@@ -10,24 +10,44 @@ import SpriteKit
 
 class Car: PhysicalObject {
 
+    let MAXCARSPEED :CGFloat = 0.08
+	var accelerate: Bool
+	var steerRight: Bool
+	var steerLeft: Bool
+	
 	init(spawnPosition: CGPoint) {
 		
 		/* Temporary initialization.. will have further customization for different classes of cars */
 		let carTexture = SKTexture(imageNamed: "BasicCar")
-		let carWidth = CGFloat(screenSize.width/15)
+		let carWidth = CGFloat(screenSize.width/20)
 		let carScale = CGFloat(183.0/140.0)
-		let carSize = CGSize(width: carWidth, height: (carWidth*carScale))
+		let carSize = CGSize(width: (carWidth*carScale), height: carWidth)
+		
+		// Initialize movement variables
+    	self.accelerate = false
+    	self.steerRight = false
+    	self.steerLeft = false
+		
 		super.init(texture: carTexture, color: UIColor.clearColor(), size: carSize)
+		
+		
+		self.speed = CGFloat(MAXCARSPEED)
 		self.position = spawnPosition
 		self.name = "car"
-		self.physicsBody = SKPhysicsBody(rectangleOfSize: carSize)
-		self.objectMass = CGFloat(0.10)
 		
 		//Physics Setup
-		self.physicsBody?.categoryBitMask = PhysicsCategory.Car
-		self.physicsBody?.contactTestBitMask = PhysicsCategory.Boards | PhysicsCategory.Car
-		self.physicsBody?.categoryBitMask = PhysicsCategory.Car
+		//self.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(origin: spawnPosition, size: carSize) )
+		let carPhysicsSize = CGSize(width: self.size.width, height: self.size.height)
+		self.physicsBody = SKPhysicsBody(rectangleOfSize: carPhysicsSize)
+		self.objectMass = CGFloat(20)
+		self.physicsBody?.friction = 0.5
+		physicsBody?.dynamic = true // Default is true
+
 		
+		self.physicsBody?.categoryBitMask = PhysicsCategory.Car
+		self.physicsBody?.contactTestBitMask = PhysicsCategory.Boards | PhysicsCategory.Car | PhysicsCategory.Ball
+		
+
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
--- NCSoccer/NCSoccer/NCSoccer/SoccerScene.swift
+++ NCSoccer/NCSoccer/NCSoccer/SoccerScene.swift
@@ -12,18 +12,19 @@ import SpriteKit
 
 class SoccerScene: SKScene, SKPhysicsContactDelegate {
 	
+	var car1: Car! = nil
 	
     override func didMoveToView(view: SKView) {
 		
 		let screenRect = UIScreen.mainScreen().bounds
 		
-		// Setup Physics
-		physicsWorld.gravity = CGVectorMake(0, -1.0)
+		// Physics
+		physicsWorld.gravity = CGVectorMake(0, 0)
 		physicsWorld.contactDelegate = self
 		
 		// Create boundary for field
-		let FieldBoundary = CGRect(x: 0, y: 0, width: screenRect.width, height: screenRect.height)
-		let boundaryBody = SKPhysicsBody(edgeLoopFromRect: FieldBoundary)
+		//let FieldBoundary = CGRect(x: 0, y: 0, width: screenRect.width, height: screenRect.height)
+		let boundaryBody = SKPhysicsBody(edgeLoopFromRect: screenRect)
 		self.physicsBody = boundaryBody
 		self.physicsBody?.categoryBitMask = PhysicsCategory.Boards
 		self.physicsBody?.contactTestBitMask = PhysicsCategory.Ball | PhysicsCategory.Car
@@ -36,17 +37,114 @@ class SoccerScene: SKScene, SKPhysicsContactDelegate {
 		
 		// Add a car
 		let carSpawnSpot = CGPoint(x: frame.midX, y: frame.midY)
-		let car1 = Car(spawnPosition: carSpawnSpot)
+		car1 = Car(spawnPosition: carSpawnSpot)
 		addChild(car1)
 		
     }
 	
+	// MARK: - Touch Handling
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
+		/* Test code splits up screen to act as controls */
+		for Touch in touches {
+			if Touch.locationInNode(self).x < frame.midX { // touch on left side of screen
+				if Touch.locationInNode(self).x < frame.midX/2.0 { // touch on left quarter of screen // turn left
+					car1.steerLeft = true
+					car1.steerRight = false
+				} else { // Touch on 2nd quarter of screen // turn right
+					car1.steerRight = true
+					car1.steerLeft = false
+				}
+			} else { // Touch on right half of screen
+				car1.accelerate = true
+			}
+		}
+	}
+	
+	override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		var accelerating = 0
+		var turnRight = 0
+		var turnLeft = 0
+		for Touch in touches {
+			if Touch.locationInNode(self).x < frame.midX { // touch on left side of screen
+    			if Touch.locationInNode(self).x < frame.midX/2.0 { // touch on left quarter of screen
+					turnLeft += 1
+    			} else { // Touch on 2nd quarter of screen
+					turnRight += 1
+    			}
+    		} else { // Touch on right half of screen
+				accelerating += 1
+    		}
+    	}
+		
+		if (turnRight == 0) && (turnLeft > 0) {
+			car1.steerLeft = true
+			car1.steerRight = false
+		} else if (turnLeft == 0) && (turnRight > 0) {
+			car1.steerLeft = false
+			car1.steerLeft = true
+		} else {
+			car1.steerLeft = false
+			car1.steerRight = false
+		}
+		
+		if accelerating > 0 {
+			car1.accelerate = true
+		} else {
+			car1.accelerate = false
+		}
 		
 	}
 	
+	override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		for Touch in touches {
+			if Touch.locationInNode(self).x < frame.midX { // touch on left side of screen
+    			if Touch.locationInNode(self).x < frame.midX/2.0 { // touch on left quarter of screen
+    				car1.steerLeft = false
+    			} else { // Touch on 2nd quarter of screen
+    				car1.steerRight = true
+    			}
+    		} else { // Touch on right half of screen
+				car1.accelerate = false
+    		}
+    	}
+		
+	}
+	
+	// MARK: - Frame Cycle
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+		
+		// Really Strange syntax. Blocks are confusing but they're needed here for the
+		// enumerateChildNodesWithName() function. Essentially a block is a function.
+		// What I'm doing here is declaring a function that has parameter types SKNode!
+		// and UnsafeMutablePointer<ObjCBool> and return type void. Then I'm setting that
+		// equal to an actual function with with specific parameter names. Idk it just works lol
+		// Pretty sure this is like a 1 to 1 translation to a C function pointer.
+		let updateNode: (SKNode!, UnsafeMutablePointer<ObjCBool>) -> Void = {
+			(node, NilLiteralConvertible) -> Void in
+			
+			// Update car rotation
+			if (self.car1.steerLeft) {
+				self.car1.physicsBody?.applyTorque(CGFloat(10))
+			} else if (self.car1.steerRight) {
+				self.car1.physicsBody?.applyTorque(CGFloat(-10))
+			}
+			print(self.car1.position)
+			
+			//car.physicsBody?.velocity = CGVectorMake(CGFloat(cos(self.zRotation)) * car.speed, CGFloat(sin(self.zRotation)) * car.speed)
+			//var carVelocity = sqrt(int(car.physicsBody?.velocity.dx << 1) + int(car.physicsBody?.velocity.dy << 1))
+			let carVelocity = sqrtf(pow(Float((self.car1.physicsBody?.velocity.dx)!), 2) + pow(Float((self.car1.physicsBody?.velocity.dx)!),2))
+			
+			if (self.car1.accelerate) {
+    			if (carVelocity < 60) {
+        			self.car1.physicsBody?.applyImpulse(CGVectorMake(CGFloat(cos(self.zRotation)) * self.car1.speed, CGFloat(sin(self.zRotation)) * self.car1.speed))
+    			}
+    		}
+		}
+		
+		self.enumerateChildNodesWithName("car", usingBlock: updateNode)
+		
     }
 }
+
